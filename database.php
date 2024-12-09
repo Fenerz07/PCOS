0fea2c6e59ddc6fbf722648c13f52e1f54770141
hugo <hugom2005@free.fr>
2024-12-09 12:24:42 +0100

    Initial commit

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+try {
+    $pdo = new PDO(
+        'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_DATABASE'],
+        $_ENV['DB_USERNAME'],
+        $_ENV['DB_PASSWORD']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base de données.";
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
